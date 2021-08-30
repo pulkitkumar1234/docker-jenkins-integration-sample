1b1f10b0f0529eac1306e75e41855fd8cf6a0e8f
Pulkit Kumar <pj.pulkitjhunjhunwala@gmail.com>
2021-08-30 19:31:12 +0530

    First commint

---
+++ src/test/java/com/docker/jenkins/example/SpringBootJenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
