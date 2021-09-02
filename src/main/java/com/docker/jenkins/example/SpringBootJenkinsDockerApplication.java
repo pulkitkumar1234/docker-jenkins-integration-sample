f4afc026e3bfd34c8d5579aee8e9bce89a2e671a
Pulkit Kumar <pj.pulkitjhunjhunwala@gmail.com>
2021-09-02 20:40:38 +0530

    test

--- src/main/java/com/docker/jenkins/example/SpringBootJenkinsDockerApplication.java
+++ src/main/java/com/docker/jenkins/example/SpringBootJenkinsDockerApplication.java
@@ -2,10 +2,17 @@ package com.docker.jenkins.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootJenkinsDockerApplication {
 
+	@GetMapping("/message")
+	public String get(){
+		return "Test String";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsDockerApplication.class, args);
 	}
