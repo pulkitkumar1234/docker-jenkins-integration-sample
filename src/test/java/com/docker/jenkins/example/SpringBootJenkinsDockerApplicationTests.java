19cd1073b98174b917763f39e5f7eee8ce2c6c2b
Pulkit Kumar <pj.pulkitjhunjhunwala@gmail.com>
2021-09-02 20:42:35 +0530

    test

--- src/test/java/com/docker/jenkins/example/SpringBootJenkinsDockerApplicationTests.java
+++ src/test/java/com/docker/jenkins/example/SpringBootJenkinsDockerApplicationTests.java
@@ -3,11 +3,14 @@ package com.docker.jenkins.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringBootJenkinsDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
